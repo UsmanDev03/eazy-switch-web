f8c822ad8a2de0a27cc5345e51c4cb51ea4fc47e
Usman <usmanhadier03@gmail.com>
2026-04-17 13:08:29 +0500

    latest get

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -40,7 +40,6 @@ export function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-12">
           {/* Brand Column */}
-          {/* Brand Column */}
           <div className="lg:col-span-2">
             <Link
               href="/"
